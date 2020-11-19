23ed424b59af4098f3aebc604883d8060dc6913c
dashdavid13 <davidamedina619@gmail.com>
2020-11-19 17:02:47 -0500

    trying bonus question

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,19 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :title, presence: true, uniqueness: true 
+    validates :alias, presence: true, uniqueness: true 
+    def full_name
+        "#{self.first_name.capitalize} #{self.last_name.capitalize}"
+    end
+    
+    #def exist
+       # Employee.all.each do |e|
+           # print "hi"
+            # if Employee.all.include?(e.alias)
+            #     puts "Sorry this alias has been used already!!"
+            # elsif Employee.all.include?(e.title)
+            #     puts "Title has been taken!!"
+            # end
+        #end
+    #end
 end
